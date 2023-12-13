b4ef942fffa46d3ed252255ef58ce058d8586b20
martyasbutkus <noobtuberis1990@gmail.com>
2023-12-13 09:22:04 +0200

    ada

--- script.js
+++ script.js
@@ -2,124 +2,149 @@ const thememusic = document.getElementById("thememusic")
 thememusic.volume = 0.1
 const shot = document.getElementById("shot")
 shot.volume = 0.2
-const shotmiss = document.getElementById("shotmiss");
+const shotmiss = document.getElementById("shotmiss")
 shotmiss.volume = 0.1
 
 document.addEventListener("DOMContentLoaded", function() {
-    const musicCheckbox = document.getElementById("musicCheckbox")
-    const thememusic = document.getElementById("thememusic")
-    const musicPreference = localStorage.getItem("musicPreference")
+    const musicCheckbox = document.getElementById("musicCheckbox");
+    const thememusic = document.getElementById("thememusic");
+    const musicPreference = localStorage.getItem("musicPreference");
 
     if (musicPreference === null || musicPreference === "true") {
-        thememusic.play()
-        musicCheckbox.checked = true
+        thememusic.play();
+        musicCheckbox.checked = true;
     }
     musicCheckbox.addEventListener("change", function() {
         if (musicCheckbox.checked) {
-            thememusic.play()
+            thememusic.play();
         } else {
-            thememusic.pause()
+            thememusic.pause();
         }
-        localStorage.setItem("musicPreference", musicCheckbox.checked)
-    })
-})
+        localStorage.setItem("musicPreference", musicCheckbox.checked);
+    });
+});
+
 class AimTraining {
     constructor() {
-        this.tabs = document.getElementsByClassName("tab")
-        this.buttonsContainer = document.querySelector(".menu")
-        this.buttons = document.querySelectorAll("button")
-        this.score = 0
-        this.timer = 5
-        this.dot
-        this.gameInterval
-        this.buttonsContainer.style.display = "flex"
+        this.tabs = document.getElementsByClassName("tab");
+        this.buttonsContainer = document.querySelector(".menu");
+        this.buttons = document.querySelectorAll("button");
+        this.score = 0;
+        this.timer = 5;
+        this.dot;
+        this.gameInterval;
+        this.buttonsContainer.style.display = "flex";
+        this.highestScore = localStorage.getItem("highestScore") || 0;
         this.buttons.forEach((button, index) => {
             button.addEventListener("click", (event) => {
-                const tabId = this.tabs[index].id
-                this.changeTab(tabId, event)
-            })
-        })
+                const tabId = this.tabs[index].id;
+                this.changeTab(tabId, event);
+            });
+        });
     }
+
     changeTab(id, event) {
         for (const tab of this.tabs) {
-            tab.style.display = "none"
+            tab.style.display = "none";
         }
-        document.getElementById(id).style.display = "flex"
-        this.buttonsContainer.style.display = "none"
+        document.getElementById(id).style.display = "flex";
+        this.buttonsContainer.style.display = "none";
         for (const button of this.buttons) {
-            button.classList.remove("active")
+            button.classList.remove("active");
         }
-        event.currentTarget.classList.add("active")
+        event.currentTarget.classList.add("active");
     }
+
     updateScore() {
-        document.getElementById("score").innerText = `Score: ${this.score}`
+        document.getElementById("score").innerText = `Score: ${this.score}`;
     }
+
     updateTimer() {
-        document.getElementById("timer").innerText = `Time: ${this.timer}s`
+        document.getElementById("timer").innerText = `Time: ${this.timer}s`;
     }
+
+    updateHighestScore() {
+        document.getElementById("highest").innerText = `Highest Score: ${this.highestScore}`;
+    }
+
+    saveHighestScore() {
+        localStorage.setItem("highestScore", this.highestScore);
+    }
+
     createDot() {
-        this.dot = document.createElement("div")
-        this.dot.className = "dot"
-        const maxWidth = window.innerWidth - 20
-        const maxHeight = window.innerHeight - 20
-        this.dot.style.left = Math.random() * maxWidth + "px"
-        this.dot.style.top = Math.random() * maxHeight + "px"
+        this.dot = document.createElement("div");
+        this.dot.className = "dot";
+        const maxWidth = window.innerWidth - 20;
+        const maxHeight = window.innerHeight - 20;
+        this.dot.style.left = Math.random() * maxWidth + "px";
+        this.dot.style.top = Math.random() * maxHeight + "px";
         this.dot.addEventListener("click", () => {
-            const shot = document.getElementById("shot")
-            this.dot.remove()
-            this.score++
-            this.updateScore()
-            setTimeout(() => this.createDot(), 1)
-            shot.play()
-        })
-        document.body.addEventListener("click", (event) => {
+            const shot = document.getElementById("shot");
+            this.dot.remove();
+            this.score++;
+            this.updateScore();
+            setTimeout(() => this.createDot(), 1);
+            shot.play();
+        });
+        const bodyClickHandler = (event) => {
             if (event.target !== this.dot) {
-                const shotmiss = document.getElementById("shotmiss")
-                shotmiss.play()
+                const shotmiss = document.getElementById("shotmiss");
+                shotmiss.play();
             }
-        })
-        document.body.appendChild(this.dot)
+        };
+        document.body.addEventListener("click", bodyClickHandler);
+        this.bodyClickHandler = bodyClickHandler;
+        document.body.appendChild(this.dot);
     }
+
     hideDots() {
-        const dots = document.querySelectorAll(".dot")
+        const dots = document.querySelectorAll(".dot");
         dots.forEach((dot) => {
-            dot.style.display = "none"
-        })
+            dot.style.display = "none";
+        });
     }
 
     stopGame() {
-        clearInterval(this.gameInterval)
-        this.hideDots()
-        this.buttonsContainer.style.display = "flex"
-        this.dot = null
+        clearInterval(this.gameInterval);
+        this.hideDots();
+        this.buttonsContainer.style.display = "flex";
+        this.dot = null;
+        document.body.removeEventListener("click", this.bodyClickHandler);
+
+        if (this.score > this.highestScore) {
+            this.highestScore = this.score;
+            this.updateHighestScore();
+            this.saveHighestScore();
+        }
     }
 
     startGame() {
         this.gameInterval = setInterval(() => {
             if (this.timer > 0) {
                 if (!this.dot) {
-                    this.createDot()
+                    this.createDot();
                 }
-                this.timer--
-                this.updateTimer()
+                this.timer--;
+                this.updateTimer();
             } else {
-                this.stopGame()
+                this.stopGame();
             }
-        }, 1000)
+        }, 1000);
     }
 
     initialize() {
-        this.timer = 60
-        this.score = 0
-        this.updateScore()
-        this.updateTimer()
-        document.querySelector(".menu").style.display = "none"
-        this.startGame()
+        this.timer = 5;
+        this.score = 0;
+        this.updateScore();
+        this.updateTimer();
+        this.updateHighestScore();
+        document.querySelector(".menu").style.display = "none";
+        this.startGame();
     }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const aimTraining = new AimTraining()
+    const aimTraining = new AimTraining();
 
-    document.getElementById("aim").addEventListener("click", () => aimTraining.initialize())
-})
+    document.getElementById("aim").addEventListener("click", () => aimTraining.initialize());
+});
